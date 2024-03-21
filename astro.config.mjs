c9dbf9e37f6fa6330cf1754c42942c8d9a47ebdb
Phil Giammattei <philgiammattei@gmail.com>
2024-03-20 22:18:52 -0400

    dynamic routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com" //site url
+  ,
+  integrations: [preact()]
+});
